250e93e023fc63b90e5a340c4b2f30f21f64e971
portifolio-html <evaldogusmaocosta@outlook.com>
2024-10-07 22:28:09 -0300

    menu pronto

---
+++ js/home/script-home.js
@@ -0,0 +1,10 @@
+
+var menu = document.getElementById("menu-open")
+
+
+function openmenu(){
+    var lista = document.getElementById("nav-bar")
+    lista.style.background="black"
+}
+
+menu.onclick(openmenu)
