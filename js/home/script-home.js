595ebb489307330f9159b6b5eee0543abfba27fe
portifolio-html <evaldogusmaocosta@outlook.com>
2024-10-09 08:50:10 -0300

    nova atualização pagina

--- js/home/script-home.js
+++ js/home/script-home.js
@@ -3,7 +3,6 @@ var menu = document.querySelector("#menu-open")
 
 menu.addEventListener("click", function(){
     var lista = document.querySelector('#nav-bar')
-    var close = document.querySelector('#menu-open')
     lista.classList.toggle("cont-list-bar-on")
-    close.classList.toggle("menu-list-close")
+    this.classList.toggle("menu-list-close")
 })
